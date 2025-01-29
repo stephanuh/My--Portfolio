bbf3f7e814ba73c2b7c012b127ebf1c9d018cde1
Papaya <stephaniesosa02@gmail.com>
2025-01-29 13:29:38 -0700

    html change logo & name

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App';
+import Error from './pages/Error';
+import About from './pages/About'
+import Portfolio from './pages/Portfolio'
+import Resume from './pages/Resume'
+import Contact from './pages/Contact'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: './Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: './Resume',
+        element: <Resume />,
+      },
+      {
+        path: './Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />,
 )
+
